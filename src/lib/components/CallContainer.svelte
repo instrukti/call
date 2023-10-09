c8e317be5ede6367e743b327feffb422439217b9
Shubhendu Madhukar <shubhendumadhukar@gmail.com>
2023-10-09 23:27:59 +0530

    feat: livekit

--- src/lib/components/CallContainer.svelte
+++ src/lib/components/CallContainer.svelte
@@ -1,17 +1,22 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, onMount } from "svelte";
   import { Avatar, Button, Card, CardBody, CardHeader, CardTitle, Dot, El, Icon, Offcanvas, OffcanvasBody, OffcanvasHeader, Status, Tooltip } from "yesvelte";
   import TimerModal from "./TimerModal.svelte";
   import ReactionDropdown from "./ReactionDropdown.svelte";
   import { isChatHidden, isCallMaximized, isWhiteboardHidden, isWhiteboardMaximized } from "../stores/visibility";
   import { slide } from "svelte/transition";
   import ChatBody from "./ChatBody.svelte";
+  import { LivekitUtils, remoteParticipants } from "../utils/livekit_utils";
+  /** @type {LivekitUtils}*/
+  let livekitUtils;
   let showChatCanvas = false;
   let isMicOn = true;
-  let isVideoOn = true;
+  let isVideoOn = false;
   let isHandRaised = false;
   let recording = false;
   let hiddenControls = false;
+  /** @type {HTMLMediaElement}*/
+  let localViewContainer;
   const toggle = (/** @type {"mic"|"video"|"hand"}*/ button) => {
     switch (button) {
       case "mic":
@@ -19,6 +24,7 @@
         break;
       case "video":
         isVideoOn = !isVideoOn;
+        isVideoOn ? livekitUtils.turnVideoOn() : livekitUtils.turnVideoOff();
         break;
       case "hand":
         isHandRaised = !isHandRaised;
@@ -30,6 +36,12 @@
   const dispatch = createEventDispatcher();
   const maximize = () => dispatch("maximize");
   const minimize = () => dispatch("minimize");
+
+  onMount(() => {
+    setTimeout(() => {
+      livekitUtils = new LivekitUtils(localViewContainer, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTY5NjAyMzgsImlzcyI6IkFQSUJDNnBpZmZCcUhFSyIsIm5iZiI6MTY5Njg3MzgzOCwic3ViIjoicXVpY2tzdGFydCB1c2VyIHVsNTdvaCIsInZpZGVvIjp7ImNhblB1Ymxpc2giOnRydWUsImNhblB1Ymxpc2hEYXRhIjp0cnVlLCJjYW5TdWJzY3JpYmUiOnRydWUsInJvb20iOiJxdWlja3N0YXJ0IHJvb20iLCJyb29tSm9pbiI6dHJ1ZX19.CXpszwstL0SXDELGuOl7rVX21KKTlXzjqZoWwSxt-Fs");
+    }, 500);
+  });
 </script>
 
 <Card class="m-4 h-full flex-grow !rounded-2xl">
@@ -90,9 +102,15 @@
   </CardHeader>
   <CardBody class="h-5/6 bg-gray-200 !p-0 relative rounded-b-2xl">
     <div class="h-full w-full fill absolute rounded-b-2xl">
-      <img src="https://media.istockphoto.com/id/1136449639/video/the-man-laughing-on-the-white-background-slow-motion.jpg?s=640x640&k=20&c=jCivJBgTODPt57y3ESNnasT4Dc8z2Semme4VmNK2EeM=" class="object-cover" alt="" />
+      <!-- svelte-ignore a11y-media-has-caption -->
+      <video class={$isCallMaximized ? "object-scale-down" : "object-cover"} bind:this={localViewContainer} autoplay muted />
+      {#if !isVideoOn}
+        <div class="w-full h-full absolute z-10 bg-slate-200 grid place-items-center">
+          <Icon name="user" color="tabler" size="9x" />
+        </div>
+      {/if}
     </div>
-    <div class="absolute z-10 h-full w-full flex flex-col p-4">
+    <div class="absolute z-20 h-full w-full flex flex-col p-4">
       <div class="flex-grow flex">
         <div class="flex flex-col flex-grow">
           <div>
@@ -142,10 +160,9 @@
           </div>
         </div>
         <div class="flex flex-col justify-between">
-          <div><Avatar shape="circle" color="azure" class="!w-20 !h-20 md:!w-32 md:!h-32 !text-3xl">JD</Avatar></div>
-          <div><Avatar shape="circle" color="warning" class="!w-20 !h-20 md:!w-32 md:!h-32 !text-3xl">SM</Avatar></div>
-          <div><Avatar shape="circle" color="green" class="!w-20 !h-20 md:!w-32 md:!h-32 !text-3xl">A</Avatar></div>
-          <div><Avatar shape="circle" color="cyan" class="!w-20 !h-20 md:!w-32 md:!h-32 !text-3xl">SO</Avatar></div>
+          {#each $remoteParticipants as participant, i}
+            <div><Avatar id={participant.sid} shape="circle" color="azure" class="!w-20 !h-20 md:!w-32 md:!h-32 !text-3xl">JD</Avatar></div>
+          {/each}
         </div>
       </div>
     </div>
@@ -165,7 +182,7 @@
     align-items: center;
     overflow: hidden;
   }
-  .fill img {
+  .fill video {
     flex-shrink: 0;
     min-width: 100%;
     min-height: 100%;
---
+++ src/lib/utils/livekit_utils.js
@@ -0,0 +1,72 @@
+import { RemoteParticipant, Room, VideoPreset, VideoPresets } from "livekit-client";
+import { get, writable } from "svelte/store";
+/** @type {import('svelte/store').Writable<VideoPreset>} */
+export const videoResolutions = writable(VideoPresets.h720);
+/** @type {import('svelte/store').Writable<RemoteParticipant[]>} */
+export const remoteParticipants = writable([]);
+export class LivekitUtils {
+  /** @type {Room} */
+  room;
+  /** @type {string} */
+  wsURL = "wss://instrukti-nf5brd5y.livekit.cloud";
+  /** @type {string} */
+  token;
+  /** @type {HTMLMediaElement} */
+  localViewContainer;
+  constructor(/** @type {HTMLMediaElement} */ localContainer, /** @type {string} */ token) {
+    this.localViewContainer = localContainer;
+    this.setLocalView();
+    this.room = new Room();
+    this.token = token;
+    this.joinRoom();
+  }
+  joinRoom = async () => {
+    await this.room.connect(this.wsURL, this.token);
+    await this.room.localParticipant.enableCameraAndMicrophone();
+    await this.room.on("trackSubscribed", this.trackSubscription);
+    console.log(this.room.participants.size);
+    videoResolutions.subscribe(() => {
+      this.turnVideoOff();
+      this.turnVideoOn();
+    });
+  };
+  leaveRoom = async () => {
+    await this.room.disconnect();
+  };
+  muteMic = async () => {
+    await this.room.localParticipant.setMicrophoneEnabled(false);
+  };
+  unMuteMic = async () => {
+    await this.room.localParticipant.setMicrophoneEnabled(true, { noiseSuppression: true, echoCancellation: true });
+  };
+  turnVideoOff = async () => {
+    if (this.room) {
+      await this.room.localParticipant.setCameraEnabled(false);
+    }
+  };
+  turnVideoOn = async () => {
+    if (this.room) {
+      await this.room.localParticipant.setCameraEnabled(true, { resolution: get(videoResolutions) });
+    }
+    this.setLocalView();
+  };
+  shareScreen = async () => {
+    await this.room.localParticipant.setScreenShareEnabled(true);
+  };
+  stopSharing = async () => {
+    await this.room.localParticipant.setScreenShareEnabled(false);
+  };
+  trackSubscription = async (/** @type {import('livekit-client').RemoteTrack} */ track, /** @type {import('livekit-client').RemoteTrackPublication} */ publication, /** @type {import('livekit-client').RemoteParticipant} */ participant) => {
+    const participants = Array.from(this.room.participants.values());
+    remoteParticipants.set(participants);
+    setTimeout(() => {
+      const parentElement = document.getElementById(participant.sid);
+      const element = track.attach();
+      parentElement.appendChild(element);
+    });
+  };
+  setLocalView = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    this.localViewContainer.srcObject = stream;
+  };
+}
