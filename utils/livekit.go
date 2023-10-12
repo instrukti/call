2324bd426d22aa81dad7aa0c2c02bc2efd035f73
Shubhendu Madhukar <shubhendumadhukar@gmail.com>
2023-10-12 11:34:49 +0530

    feat: finish get token function

--- utils/livekit.go
+++ utils/livekit.go
@@ -1,23 +1,32 @@
 package utils
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/labstack/echo/v5"
 	"github.com/livekit/protocol/auth"
+	"github.com/livekit/protocol/livekit"
+	lksdk "github.com/livekit/server-sdk-go"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/models"
 )
 
 func GetJoinToken(c echo.Context, app *pocketbase.PocketBase) error {
+	roomName := c.QueryParam("room")
+	room, err := getRoom(c, app, roomName)
+	if err != nil {
+		return err
+	}
 	authRecord, _ := c.Get(apis.ContextAuthRecordKey).(*models.Record)
 	at := auth.NewAccessToken(os.Getenv("LIVEKIT_API_KEY"), os.Getenv("LIVEKIT_API_SECRET"))
 	grant := &auth.VideoGrant{
 		RoomJoin: true,
-		Room:     "Demo Room",
+		Room:     room.Name,
 	}
 	at.AddGrant(grant).
 		SetIdentity(authRecord.Id).
@@ -26,30 +35,32 @@ func GetJoinToken(c echo.Context, app *pocketbase.PocketBase) error {
 	return c.String(http.StatusOK, token)
 }
 
-// func GetRoom(c echo.Context, app *pocketbase.PocketBase) (*livekit.Room, error) {
-// 	host := "http://localhost:7880"
-// 	roomClient := lksdk.NewRoomServiceClient(host, os.Getenv("LIVEKIT_API_KEY"), os.Getenv("LIVEKIT_API_SECRET"))
-// 	rooms, _ := roomClient.ListRooms(context.Background(), &livekit.ListRoomsRequest{})
-// 	if rooms == nil || len(rooms.GetRooms()) == 0 {
-// 		err := CreateRooms(c, app, roomClient)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-
-// 	} else {
-// 		fmt.Print(rooms.GetRooms()[0])
-// 	}
-// 	return nil, nil
-// }
-// func CreateRooms(c echo.Context, app *pocketbase.PocketBase, roomClient *lksdk.RoomServiceClient) error {
-// 	_, err := roomClient.CreateRoom(context.Background(), &livekit.CreateRoomRequest{
-// 		Name:            "Demo Room",
-// 		EmptyTimeout:    2 * 60, // 2 minutes
-// 		MaxParticipants: 2,
-// 	})
-// 	if err != nil {
-// 		fmt.Print(err)
-// 		return apis.NewApiError(500, err.Error(), nil)
-// 	}
-// 	return nil
-// }
+func getRoom(c echo.Context, app *pocketbase.PocketBase, name string) (*livekit.Room, error) {
+	host := "http://localhost:7880"
+	roomClient := lksdk.NewRoomServiceClient(host, os.Getenv("LIVEKIT_API_KEY"), os.Getenv("LIVEKIT_API_SECRET"))
+	rooms, _ := roomClient.ListRooms(context.Background(), &livekit.ListRoomsRequest{
+		Names: []string{name},
+	})
+	if rooms == nil || len(rooms.GetRooms()) == 0 {
+		room, err := createRooms(c, app, roomClient, name)
+		if err != nil {
+			return nil, err
+		}
+		return room, nil
+	} else {
+		room := rooms.GetRooms()[0]
+		return room, nil
+	}
+}
+func createRooms(c echo.Context, app *pocketbase.PocketBase, roomClient *lksdk.RoomServiceClient, name string) (*livekit.Room, error) {
+	room, err := roomClient.CreateRoom(context.Background(), &livekit.CreateRoomRequest{
+		Name:            name,
+		EmptyTimeout:    2 * 60, // 2 minutes
+		MaxParticipants: 2,
+	})
+	if err != nil {
+		fmt.Print(err)
+		return nil, apis.NewApiError(500, err.Error(), nil)
+	}
+	return room, nil
+}
